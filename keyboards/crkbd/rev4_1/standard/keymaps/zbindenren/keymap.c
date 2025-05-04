cdac4ed648a9b3dba072ed2d9a006b9b7b9d34f7
Rene Zbinden <rene.zbinden@postfinance.ch>
2025-05-04 14:00:08 +0200

    fix: first try to fix caps word for swiss layout

--- keyboards/crkbd/rev4_1/standard/keymaps/zbindenren/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/zbindenren/keymap.c
@@ -247,3 +247,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case CH_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
